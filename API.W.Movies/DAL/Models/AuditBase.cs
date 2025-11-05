8592f9a24c8cfd690b0e59e13f3fde774799d410
cjuni <cjuni@JRSOFTWAREDEV>
2025-11-05 02:00:27 -0500

    CategoriesController and CategoryService created

--- API.W.Movies/DAL/Models/AuditBase.cs
+++ API.W.Movies/DAL/Models/AuditBase.cs
@@ -9,6 +9,6 @@ namespace API.W.Movies.DAL.Models
 
         public virtual DateTime CreatedDate { get; set; }
 
-        public virtual DateTime ModifiedDate { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
     }
 }
---
+++ API.W.Movies/Services/CategoryService.cs
@@ -0,0 +1,60 @@
+﻿using API.W.Movies.DAL.Models;
+using API.W.Movies.DAL.Models.Dtos;
+using API.W.Movies.Repository.IRepository;
+using API.W.Movies.Services.IServices;
+using AutoMapper;
+
+namespace API.W.Movies.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public Task<bool> CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDto> CreateCategoryAsync(CategoryCreateDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<CategoryDto>> GetCategoriesAsync()
+        {
+            // Obtener las categorías del repositorio
+            var categories = await _categoryRepository.GetCategoriesAsync();
+
+            // Mapear toda la colección de una vez
+            return _mapper.Map<ICollection<CategoryDto>>(categories);
+        }
+
+
+        public Task<CategoryDto> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDto> UpdateCategoryAsync(int id, Category categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
